1ccf1e819006420f7708d84bf3d5b15aaf37227a
ahmedsport <ahmedsport@hotmail.com>
2022-07-04 23:49:37 +0100

    Initial commit

---
+++ Ch09/09_01/start/java_essentials/src/text_processing/TextProcessor.java
@@ -0,0 +1,16 @@
+package text_processing;
+
+public class TextProcessor {
+
+    public static void main(String[] args) {
+        countWords("I love Java");
+    }
+
+
+    public static void countWords(String text) {
+
+    }
+
+
+
+}
