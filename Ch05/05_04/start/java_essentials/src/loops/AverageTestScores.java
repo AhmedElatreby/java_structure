673bfe2ed625449a1bbb8922bc50cd52fa3265c4
ahmedsport <ahmedsport@hotmail.com>
2022-07-05 16:55:34 +0100

    nested for loop

--- Ch05/05_04/start/java_essentials/src/loops/AverageTestScores.java
+++ Ch05/05_04/start/java_essentials/src/loops/AverageTestScores.java
@@ -11,12 +11,24 @@ public class AverageTestScores {
     public static void main(String[] args){
 
         //Initialize what we know
-        int numberOfStudents = 24;
+        int numberOfStudents = 5;
         int numberOfTests = 4;
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        //Process all students
+        for (int i = 0; i < numberOfStudents; i++) {
+            double total = 0;
+
+            //process student's test score
+            for (int j = 0; j < numberOfTests; j++) {
+                System.out.println("Please enter score for test #" + (j+1));
+                double score = scanner.nextDouble();
+                total = total + score;
+            }
+            double average = total / numberOfTests;
+            System.out.println("The test average for student #" + (i+1) + " is " + average);
+        }
 
         scanner.close();
     }
