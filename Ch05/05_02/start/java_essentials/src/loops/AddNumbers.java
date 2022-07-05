58b2995f679ca6f3e531a0631c4da2a143367f43
ahmedsport <ahmedsport@hotmail.com>
2022-07-05 16:15:34 +0100

    Do while

--- Ch05/05_02/start/java_essentials/src/loops/AddNumbers.java
+++ Ch05/05_02/start/java_essentials/src/loops/AddNumbers.java
@@ -14,6 +14,23 @@ public class AddNumbers {
 
         Scanner scanner = new Scanner(System.in);
 
+        int runAgain = 0;
+
+        do {
+            System.out.println("Enter the first number");
+            double number1 = scanner.nextDouble();
+
+            System.out.println("Enter the second number");
+            double number2 = scanner.nextDouble();
+
+            double sum = number1 + number2;
+            System.out.println("The sum is " + sum);
+
+            System.out.println("Would you lie to start again? Enter 1 for yes and 2 for no");
+            runAgain = scanner.nextInt();
+
+        }while (runAgain == 1);
+
 
         scanner.close();
     }
