73cfab04072288071f63a61096d71ca8e14da4f2
ahmedsport <ahmedsport@hotmail.com>
2022-07-06 11:31:02 +0100

    constructors

--- Ch07/07_02/start/java_essentials/src/objects/Rectangle.java
+++ Ch07/07_02/start/java_essentials/src/objects/Rectangle.java
@@ -4,31 +4,36 @@ public class Rectangle {
     private double length;
     private double width;
 
-    public Rectangle(){
-
+    public Rectangle() {
+        length = 0;
+        width = 0;
+    }
+    public Rectangle(double length, double width){
+        setLength(length);
+        setWidth(width);
     }
 
-    public double calculatePerimeter(){
+    public double calculatePerimeter() {
         return (2 * length) + (2 * width);
     }
 
-    public double calculateArea(){
+    public double calculateArea() {
         return length * width;
     }
 
-    public double getLength(){
+    public double getLength() {
         return length;
     }
 
-    public void setLength(double length){
+    public void setLength(double length) {
         this.length = length;
     }
 
-    public double getWidth(){
+    public double getWidth() {
         return width;
     }
 
-    public void setWidth(double width){
+    public void setWidth(double width) {
         this.width = width;
     }
 }
