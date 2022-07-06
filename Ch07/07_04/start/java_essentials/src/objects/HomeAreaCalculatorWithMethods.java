68350cefd9425eeb9b16b9b3411fcef31ee7913e
ahmedsport <ahmedsport@hotmail.com>
2022-07-06 11:56:19 +0100

    objects as method parameters

--- Ch07/07_04/start/java_essentials/src/objects/HomeAreaCalculatorWithMethods.java
+++ Ch07/07_04/start/java_essentials/src/objects/HomeAreaCalculatorWithMethods.java
@@ -6,5 +6,10 @@ public class HomeAreaCalculatorWithMethods {
 
         Rectangle kitchen = new Rectangle(200, 400);
         Rectangle bathroom = new Rectangle(300, 700);
+        double area = calculateTotalArea(kitchen,bathroom);
+        System.out.println("The total area is: " + String.format("%.2f", area));
+    }
+    public static double calculateTotalArea(Rectangle rectangle1, Rectangle rectangle2){
+        return rectangle1.calculateArea() + rectangle2.calculateArea();
     }
 }
